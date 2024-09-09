872bfc4c7419b4827a6404691a1cdd374b80bdc6
AlexanderArrr <alexanderaretzger@gmail.com>
2024-09-09 17:46:51 +0200

    WASD movement done, Sprite Groups added

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),display=0)
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
+            
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        
         pygame.display.flip()
 
         # Limit the framerate to 60 FPS
